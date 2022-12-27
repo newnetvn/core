daac361d968ba294667f1b2c0c0dce89a3e8258d
Kim Trien Nguyen <kimtrien@gmail.com>
2022-12-27 10:38:44 +0700

    remove static file

--- public/newnet-admin/plugins/tinymce/plugins/codemirror/langs/vi.js
+++
@@ -1,8 +0,0 @@
-tinymce.addI18n('vi',{
-	'HTML source code': 'Mã HTML',
-	'Start search': 'Tìm kiếm',
-	'Find next': 'Kết quả tiếp theo',
-	'Find previous': 'Kết quả trước đó',
-	'Replace': 'Thay thế',
-	'Replace all': 'Thay thế toàn bộ'
-});
